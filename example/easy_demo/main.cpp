789df898549a57c7f7e339cba9ef14c5b6873bba
Arew <noobterr@yandex.ru>
2018-08-15 15:05:23 +0300

    Fix example issues

--- example/easy_demo/main.cpp
+++ example/easy_demo/main.cpp
@@ -1,12 +1,51 @@
-#include <QCoreApplication>
-#include "signaldemo.h"
+#include "signals.h"
+#include <iostream>
+#include <string>
 
-int main(int argc, char *argv[])
+using Callback = void(const std::string&);
+
+class A
 {
-    QCoreApplication a(argc, argv);
+public:
+    void generateSignal()
+    {
+        emit signal("Hello, world!");
+    }
 
-    SignalDemo demo;
-    demo.start();
+public signals:
+    es::Signal<Callback> signal;
+};
 
-    return a.exec();
+class B
+{
+public slots:
+    void onSignal(const std::string& str)
+    {
+        std::cout << str << " from method" << std::endl;
+    }
+};
+
+void onSignal(const std::string& str)
+{
+    std::cout << str << " from function" << std::endl;
+}
+
+int main()
+{
+    A a;
+    B b;
+
+    // member pointer
+    es::Connector::connect(a.signal, b, &B::onSignal);
+
+    // function
+    es::Connector::connect(a.signal, &onSignal);
+
+    // lambda
+    es::Connector::connect(a.signal, [](const std::string& str) {
+        std::cout << str << " from lambda" <<std::endl;
+    });
+
+    a.generateSignal();
+    return 0;
 }
